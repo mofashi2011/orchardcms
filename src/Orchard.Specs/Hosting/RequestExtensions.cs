dcf37bd19cd9a20375bc009e1f21f5cf75538b9a
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-04-14 18:22:23 -0700

    Adding a specific component to orchetrate the creation of a shell context

---
+++ src/Orchard.Specs/Hosting/RequestExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Hosting;
+using Orchard.Specs.Util;
+
+namespace Orchard.Specs.Hosting
+{
+    public static class RequestExtensions
+    {
+        public static RequestDetails SendRequest(this WebHost webHost, string urlPath)
+        {
+            var physicalPath = Bleroy.FluentPath.Path.Get(webHost.PhysicalDirectory);
+
+            var details = new RequestDetails
+            {
+                Page = physicalPath
+                    .Combine(urlPath.TrimStart('/', '\\'))
+                    .GetRelativePath(physicalPath)
+            };
+
+            webHost.Execute(() =>
+            {
+                var output = new StringWriter();
+                HttpRuntime.ProcessRequest(new Worker(details, output));
+                details.ResponseText = output.ToString();
+            });
+
+            return details;
+        }
+
+        class Worker : SimpleWorkerRequest
+        {
+            private readonly RequestDetails _details;
+            private readonly TextWriter _output;
+
+            public Worker(RequestDetails details, TextWriter output)
+                : base(details.Page, details.Query, output)
+            {
+                _details = details;
+                _output = output;
+            }
+
+            public override void SendStatus(int statusCode, string statusDescription)
+            {
+                _details.StatusCode = statusCode;
+                _details.StatusDescription = statusDescription;
+
+                base.SendStatus(statusCode, statusDescription);
+            }
+
+            public override void SendResponseFromFile(string filename, long offset, long length)
+            {
+                _output.Write(File.ReadAllText(filename));
+            }
+        }
+    }
+}
+
---
+++ src/Orchard/Environment/DefaultOrchardHost.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Web;
+using System.Collections.Generic;
+using Orchard.Environment.Configuration;
+using Orchard.Environment.ShellBuilders;
+using Orchard.Environment.Topology;
+using Orchard.Environment.Topology.Models;
+using Orchard.Extensions;
+using Orchard.Mvc;
+using Orchard.Mvc.ViewEngines;
+
+namespace Orchard.Environment {
+    public class DefaultOrchardHost : IOrchardHost {
+        private readonly IContainerProvider _containerProvider;
+        private readonly ControllerBuilder _controllerBuilder;
+        private readonly IEnumerable<IShellContainerFactory_Obsolete> _shellContainerFactories;
+
+        private readonly ITenantManager _tenantManager;
+        private IOrchardShell _current;
+
+
+        public DefaultOrchardHost(
+            IContainerProvider containerProvider,
+            ITenantManager tenantManager,
+            ITopologyDescriptorCache topologyDescriptorCache,
+            ICompositionStrategy compositionStrategy,
+            IShellContainerFactory shellContainerFactory,
+            ControllerBuilder controllerBuilder,
+            IEnumerable<IShellContainerFactory_Obsolete> shellContainerFactories) {
+            _containerProvider = containerProvider;
+            _tenantManager = tenantManager;
+            _controllerBuilder = controllerBuilder;
+            _shellContainerFactories = shellContainerFactories;
+        }
+
+
+        public IOrchardShell Current {
+            get { return _current; }
+        }
+
+
+        void IOrchardHost.Initialize() {
+            ViewEngines.Engines.Insert(0, LayoutViewEngine.CreateShim());
+            _controllerBuilder.SetControllerFactory(new OrchardControllerFactory());
+            ServiceLocator.SetLocator(t => _containerProvider.RequestLifetime.Resolve(t));
+
+            CreateAndActivateShell();
+        }
+
+        void IOrchardHost.Reinitialize() {
+            _current = null;
+            //CreateAndActivateShell();
+        }
+
+
+        void IOrchardHost.BeginRequest() {
+            BeginRequest();
+        }
+
+        void IOrchardHost.EndRequest() {
+            EndRequest();
+        }
+
+        IStandaloneEnvironment IOrchardHost.CreateStandaloneEnvironment(ShellSettings shellSettings) {
+            var shellContainer = CreateShellContainer(shellSettings);
+            return new StandaloneEnvironment(shellContainer);
+        }
+
+        protected virtual void CreateAndActivateShell() {
+            lock (this) {
+                if (_current != null) {
+                    return;
+                }
+
+                var shellContainer = CreateShellContainer();
+                var shell = shellContainer.Resolve<IOrchardShell>();
+                shell.Activate();
+                _current = shell;
+
+                // Activate extensions inside shell container
+                HackSimulateExtensionActivation(shellContainer);
+            }
+        }
+
+        protected virtual void BeginRequest() {
+            if (_current == null) {
+                CreateAndActivateShell();
+            }
+        }
+
+        protected virtual void EndRequest() {
+            _containerProvider.EndRequestLifetime();
+        }
+
+
+        public virtual IOrchardShell CreateShell() {
+            return CreateShellContainer().Resolve<IOrchardShell>();
+        }
+
+        public virtual ILifetimeScope CreateShellContainer() {
+            var settings = _tenantManager.LoadSettings();
+            if (settings.Any()) {
+                //TEMP: multi-tenancy not implemented yet
+                var shellContainer = CreateShellContainer(settings.Single());
+                if (shellContainer != null)
+                    return shellContainer;
+            }
+            return CreateShellContainer(null);
+        }
+
+        private ILifetimeScope CreateShellContainer(ShellSettings shellSettings) {
+            foreach (var factory in _shellContainerFactories) {
+                var container = factory.CreateContainer(shellSettings);
+                if (container != null) {
+                    return container;
+                }
+            }
+            return null;
+        }
+
+        private void HackSimulateExtensionActivation(ILifetimeScope shellContainer) {
+            var containerProvider = new FiniteContainerProvider(shellContainer);
+            try {
+                var requestContainer = containerProvider.RequestLifetime;
+
+                var hackInstallationGenerator = requestContainer.Resolve<IHackInstallationGenerator>();
+                hackInstallationGenerator.GenerateActivateEvents();
+            }
+            finally {
+                containerProvider.EndRequestLifetime();
+            }
+        }            
+
+    }
+}
