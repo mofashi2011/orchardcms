fb90c1fe8188ab532efc44363cd9ccabe3372f9f
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-04-30 10:02:16 -0700

    Adjusting FileSystemStorageProvider path semantics
    All path information coming in and out is within a tenant-specific location
    The location is now an implementation detail of the component

---
+++ src/Orchard/Mvc/Html/SiteServiceExtensions.cs
@@ -0,0 +1,11 @@
+using System.Web.Mvc;
+using Orchard.Settings;
+
+namespace Orchard.Mvc.Html {
+    public static class SiteServiceExtensions {
+        public static string SiteName(this HtmlHelper html)
+        {
+            return html.Resolve<ISiteService>().GetSiteSettings().SiteName;
+        }
+    }
+}
