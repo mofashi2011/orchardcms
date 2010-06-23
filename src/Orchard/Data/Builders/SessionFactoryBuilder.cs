23978412000f3317e050d1419c78194487906fdb
gauthier <gauthier@x201s>
2010-06-23 02:25:20 -0700

    * slight extensibility spike to SessionFactoryBuilderTests * initial sanity unit testing coverage of this behaviour
    sorry for space formattings, it might get merged with changes proposed by whereslou anyway :
    
    http://social.msdn.microsoft.com/Forums/en-US/orcharddiscussions/thread/d3c8098b-1d46-4949-8112-96b50c707aed?lc=1033&prof=required

--- src/Orchard/Data/Builders/SessionFactoryBuilder.cs
+++ src/Orchard/Data/Builders/SessionFactoryBuilder.cs
@@ -2,16 +2,42 @@
 using NHibernate;
 
 namespace Orchard.Data.Builders {
+
+	
     public class SessionFactoryBuilder : ISessionFactoryBuilder {
         public ISessionFactory BuildSessionFactory(SessionFactoryParameters parameters) {
             AbstractBuilder builder;
-            if (string.Equals(parameters.Provider, "SQLite", StringComparison.InvariantCultureIgnoreCase)) {
+            if (isProviderNameMatching(parameters, "SQLite")) {
                 builder = new SQLiteBuilder(parameters.DataFolder, parameters.ConnectionString);
             }
-            else {
+						else if (isProviderNameMatching(parameters, "SQLServer")) {
                 builder = new SqlServerBuilder(parameters.DataFolder, parameters.ConnectionString);
             }
+						else {
+							builder = loadBuilderByReflectionOrFail(parameters.Provider);
+						}
             return builder.BuildSessionFactory(parameters);
         }
+
+				private AbstractBuilder loadBuilderByReflectionOrFail(string providerTypeName) {
+					AbstractBuilder builder = null;
+					try {
+						var buildertype = Type.GetType(providerTypeName);
+						if (buildertype == null) {
+							var exceptionMessage = string.Format("type '{0}' cannot be loaded, make sure the qualified name is valid", providerTypeName);
+							throw new ArgumentOutOfRangeException("providerTypeName", exceptionMessage);
+						}
+						var parameterlessconstructor = buildertype.GetConstructor(new Type[] {});
+						builder = (AbstractBuilder) parameterlessconstructor.Invoke(new object[] {});
+						return builder;
+					}
+					catch (Exception e) {
+						throw new OrchardException(string.Format("wasn't able to load builder '{0}'", providerTypeName), e);
+					}
+				}
+
+    	private bool isProviderNameMatching(SessionFactoryParameters parameters, string providerNameToMatch) {
+    		return string.Equals(parameters.Provider, providerNameToMatch, StringComparison.InvariantCultureIgnoreCase);
+    	}
     }
 }
